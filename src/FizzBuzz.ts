fad20f93ccd4169bf7cafcd12ed7a2e2f7453099
Angel Sanchez <angeljackson0933@gmail.com>
2022-10-29 14:14:38 -0500

    initial commit with ejercice fizz buzz

---
+++ src/FizzBuzz.ts
@@ -0,0 +1,26 @@
+/*
+* Problem #1
+*
+* Title: FIZZBUZZ
+* Level: Easy
+* Description: Develop a program that displays on the console (with a print) the numbers from 1 to 100
+* (both included and with a line break between each print), substituting the following:
+* - Multiples of 3 for the word "fizz".
+* - Multiples of 5 for the word "buzz".
+* - Multiples of 3 and 5 at the same time for the word "fizzbuzz".
+*
+*/
+
+export const fizzBuzz = (): void => {
+    for (let i: number = 1; i <= 100; i++) {
+        console.error(`Current count: ${i}`);
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 == 0) {
+            console.log("Fizz");
+        } else {
+            console.log("Buzz");
+        }
+    }
+}
+
